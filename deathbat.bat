7cc162ad0bbd89c341697dd61adc1c064ef1375f
Lane Garland <laneg101@gmail.com>
2012-04-13 09:39:11 -0700

    Further streamlined OS detection. Marked XP systems for OS specific tools. Added SFC Tool. Added SFC Log finder. Removed test modification. Added pauses after a couple tools to allow results viewing.
    Signed-off-by: Lane Garland <laneg101@gmail.com>

--- deathbat.bat
+++ deathbat.bat
@@ -19,18 +19,17 @@ VER | find "NT" > nul
 IF %errorlevel% EQU 0 GOTO UNSUPP
 
 VER | find "XP" > nul
-IF %errorlevel% EQU 0 GOTO toolbox
+IF %errorlevel% EQU 0 SET Is_xp=1
 
 VER | find "Vista" > nul
-IF %errorlevel% EQU 0 GOTO toolbox
+IF %errorlevel% EQU 0 SET Is_xp=0
 
 VER | find "7" > nul
-IF %errorlevel% EQU 0 GOTO toolbox
+IF %errorlevel% EQU 0 SET Is_xp=0
 
-::Detection failure::
-ECHO Unknown OS! The tool will now close.
-PAUSE
-GOTO :EOF
+IF %errorlevel% NEQ 0 GOTO UNSUPP
+
+GOTO toolbox
 
 :UNSUPP
 ECHO OS Unsupported. The tools will not run for your safety.
@@ -48,12 +47,16 @@ ECHO.
 ECHO.
 ECHO 1. CD/DVD Drive registry fixer
 ECHO 2. Reset HP Recovery Media creation software
-ECHO 3. Quit
+ECHO 3. Start Windows Secure File Checker
+ECHO 4. Create the SFC log for Vista and 7
+ECHO 5. Quit
 SET menu_option=""
 SET /p menu_option= Please select an option: 
 IF %menu_option%==1 GOTO CD_REG_FIX
 IF %menu_option%==2 GOTO HP_MEDIA
-IF %menu_option%==3 GOTO EOF
+IF %menu_option%==3 GOTO SFC
+IF %menu_option%==4 GOTO SFC_LOG
+IF %menu_option%==5 GOTO EOF
 ECHO Not a valid option, please choose again.
 GOTO toolbox
 
@@ -62,7 +65,7 @@ GOTO toolbox
 CLS
 ECHO This tool will remove the registry keys responsible for causing Windows
 ECHO to fail to load the drivers for CD and DVD drives. No side effects known.
-ECHO Restart to enable/disable drive to see the results.
+ECHO Restart or enable/disable drive to see the results.
 ECHO.
 ECHO Do you want to run this tool?
 ECHO 1. Yes
@@ -75,6 +78,7 @@ IF not %menu_option%==1 IF not %menu_option%==2 GOTO toolbox
 
 REG DELETE HKLM\SYSTEM\CurrentControlSet\Control\Class\{4D36E965-E325-11CE-BFC1-08002BE10318} /v UpperFilters /f
 REG DELETE HKLM\SYSTEM\CurrentControlSet\Control\Class\{4D36E965-E325-11CE-BFC1-08002BE10318} /v LowerFilters /f
+PAUSE
 GOTO toolbox
 
 :HP_MEDIA
@@ -95,9 +99,52 @@ IF not %menu_option%==1 IF not %menu_option%==2 GOTO toolbox
 
 mkdir C:\recovery_temp
 move D:\hp\CDCreatorLog\*.* C:\recovery_temp
+PAUSE
 GOTO toolbox
 
-::test modification::
+:SFC
+::Windows Secure File Checker helper tool::
+CLS
+ECHO This tool will start the Windows Secure File Checker for your OS.
+ECHO This will verify that various critical Windows files are in their
+ECHO original state, unmodified and unmoved. If they are changed it will
+ECHO try to replace the damaged file with a clean copy. XP may prompt for
+ECHO your install disc.
+ECHO.
+ECHO Do you want to run this tool?
+ECHO 1. Yes
+ECHO 2. No
+SET menu_option=""
+SET /p menu_option= Select an option: 
+IF %menu_option%==1 ECHO Running...
+IF %menu_option%==2 GOTO toolbox
+IF not %menu_option%==1 IF not %menu_option%==2 GOTO toolbox
+
+IF %Is_xp%==1 START START sfc.exe /scannow
+IF %Is_xp%==0 START sfc /scannow
+GOTO toolbox
+
+:SFC_LOG
+::Retrieve the SFC log in Vista and 7::
+CLS
+ECHO This will retrieve the SFC log for Windows Vista and Windows 7
+ECHO and place it in a text file on the current user's desktop. This
+ECHO log is useful for reviewing files that were repaired or not
+ECHO repairable.
+ECHO NOTE: Does not work in XP. Your SFC log is in your Event Viewer.
+ECHO.
+ECHO Do you want to run this tool?
+ECHO 1. Yes
+ECHO 2. No
+SET menu_option=""
+SET /p menu_option= Select an option: 
+IF %menu_option%==1 ECHO Running...
+IF %menu_option%==2 GOTO toolbox
+IF not %menu_option%==1 IF not %menu_option%==2 GOTO toolbox
+
+IF %Is_xp%==1 GOTO toolbox
+findstr /c:"[SR]" %windir%\Logs\CBS\CBS.log >%userprofile%\Desktop\sfcdetails.txt
+GOTO toolbox
 
 :EOF
 exit
