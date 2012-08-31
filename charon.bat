bd16cd0e807580c9c4e28fb94e0b3daa2d4e047e
Lane Garland <laneg101@gmail.com>
2012-08-30 17:12:53 -0700

    Adding 8 Detection
    Windows 8 detection is now working. A few functions of Charon still need
    tested in 8, and some of the menus need some editing. 8 support will not
    go out of the experimental section until those issues are resolved.

--- charon.bat
+++ charon.bat
@@ -22,11 +22,8 @@ IF %ERRORLEVEL% EQU 0 SET det_os=vista
 ver | findstr /i "6\.1\." > nul
 IF %ERRORLEVEL% EQU 0 SET det_os=7
 
-::Change the following to the proper version string for 8 support.
-::Also, test DLL bomber in 8.
-::
-::ver | findstr /i "windows 8 string" > nul
-::IF %ERRORLEVEL% EQU 0 SET det_os=8
+ver | findstr /i "6\.2\." > nul
+IF %ERRORLEVEL% EQU 0 SET det_os=8
 
 IF %det_os%==unsupp (
 	GOTO UNSUPP
